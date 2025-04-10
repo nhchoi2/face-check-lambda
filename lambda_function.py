422e0406153df499ec8fbce514e993a3227b10c3
nhchoi2 <choi1278@gmail.com>
2025-04-10 15:43:31 +0900

    1

---
+++ lambda_function.py
@@ -0,0 +1,29 @@
+import json
+import boto3
+
+def lambda_handler(event, context):
+    rekognition = boto3.client('rekognition')
+
+    # 예시: S3 버킷 + 이미지 파일 이름 하드코딩 (테스트용)
+    bucket = "your-bucket-name"
+    image_key = "test.jpg"  # 버킷에 미리 올려둔 이미지
+
+    try:
+        response = rekognition.detect_faces(
+            Image={
+                'S3Object': {
+                    'Bucket': bucket,
+                    'Name': image_key
+                }
+            },
+            Attributes=['ALL']
+        )
+        return {
+            'statusCode': 200,
+            'body': json.dumps(response['FaceDetails'])
+        }
+    except Exception as e:
+        return {
+            'statusCode': 500,
+            'body': json.dumps(str(e))
+        }
